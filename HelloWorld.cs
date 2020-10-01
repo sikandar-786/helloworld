58b4d7215369ea83fe22abad763aeddd12e403b8
codetravellearn <43231383+codetravellearn@users.noreply.github.com>
2020-10-01 17:21:05 +0530

    Hello World in C#

---
+++ HelloWorld.cs
@@ -0,0 +1,14 @@
+using System; 
+  
+namespace HelloWorldApp { 
+      
+    class Geeks { 
+          
+        static void Main(string[] args) { 
+              
+            Console.WriteLine("Hello World!"); 
+              
+            Console.ReadKey(); 
+        } 
+    } 
+} 
